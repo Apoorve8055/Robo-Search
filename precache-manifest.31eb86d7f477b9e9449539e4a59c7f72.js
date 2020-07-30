f2844680ff84cd81095174b88a587836f9bfdbc8
Apoorv <vapoorve@gmail.com>
2020-07-30 20:57:38 +0530

    Updates

---
+++ precache-manifest.31eb86d7f477b9e9449539e4a59c7f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4aeef30fd5c9f2a94ee45f70de9d2a7",
+    "url": "/Robo-Search/index.html"
+  },
+  {
+    "revision": "b1e2c3a786b7138c9e9b",
+    "url": "/Robo-Search/static/css/main.b016b06d.chunk.css"
+  },
+  {
+    "revision": "ff0cae826dd4e16389d6",
+    "url": "/Robo-Search/static/js/2.e2a93512.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo-Search/static/js/2.e2a93512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e2c3a786b7138c9e9b",
+    "url": "/Robo-Search/static/js/main.4777c0a1.chunk.js"
+  },
+  {
+    "revision": "19f311022db07511014d",
+    "url": "/Robo-Search/static/js/runtime-main.aced009e.js"
+  }
+]);
